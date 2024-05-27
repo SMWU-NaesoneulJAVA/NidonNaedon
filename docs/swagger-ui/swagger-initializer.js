ddbe4cd0239db06a6e39b4e248f20d1ffa136672
hoyeon <fnfn0901@sookmyung.ac.kr>
2024-05-27 17:18:07 +0900

    파일 경로 절대 경로로 수정

--- docs/swagger-ui/swagger-initializer.js
+++ docs/swagger-ui/swagger-initializer.js
@@ -5,7 +5,7 @@ window.onload = function() {
 
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
-    url: "./swagger.json",  // 로컬 JSON 파일 경로로 변경합니다.
+    url: "/NidonNaedon/docs/swagger-ui/swagger.json",  // 로컬 JSON 파일 경로로 변경합니다.
     dom_id: '#swagger-ui',
     deepLinking: true,
     presets: [

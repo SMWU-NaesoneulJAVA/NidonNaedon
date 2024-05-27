25b640908880814b1ea0cb908fbcff9f194e935e
hoyeon <fnfn0901@sookmyung.ac.kr>
2024-05-27 16:45:41 +0900

    Swagger UI 추가

---
+++ swagger-initializer.js
@@ -0,0 +1,22 @@
+// swagger-initializer.js
+
+window.onload = function() {
+  //<editor-fold desc="Changeable Configuration Block">
+
+  // the following lines will be replaced by docker/configurator, when it runs in a docker-container
+  window.ui = SwaggerUIBundle({
+    url: "./swagger.json",  // 로컬 JSON 파일 경로로 변경합니다.
+    dom_id: '#swagger-ui',
+    deepLinking: true,
+    presets: [
+      SwaggerUIBundle.presets.apis,
+      SwaggerUIStandalonePreset
+    ],
+    plugins: [
+      SwaggerUIBundle.plugins.DownloadUrl
+    ],
+    layout: "StandaloneLayout"
+  });
+
+  //</editor-fold>
+};
